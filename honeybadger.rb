bf8c43b212e1b6bd46fcb9767991e2d705350166
Amiel Martin <amiel.martin@gmail.com>
2013-03-12 14:34:41 -0700

    An inital failed attempt at a second message

--- honeybadger.rb
+++ honeybadger.rb
@@ -11,6 +11,9 @@ bot = Cinch::Bot.new do
     m.target.action "doesn't give a shit"
   end
 
+  on :message, /honeybadger.*(care|give a shit)/ do |m|
+    m.reply 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCs89JTPXOEXcbkh0gPgN24V8EffZPxC4xv_Pfetq_KnVIw4vw'
+  end
 end
 
 bot.start
